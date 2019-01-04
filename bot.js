962c6e0d0c6e58932653328848a27c7d59d8c8ff
mohamad123eru <46269702+mohamad123eru@users.noreply.github.com>
2019-01-04 21:43:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ const prefix = "!";
 client.on("ready", () => {
 let channel = client.channels.get("530799587464839201")
 setInterval(function() {
-channel.send(`NUMBER ONE [ " ${x} " ]`);
+channel.send(` *NUMBER ONE* `);
 }, 999)
 })
 
